faaa16aca6f702b8efaa61a94c029f8cf304f465
HighDivineFox <chris_3977@hotmail.com>
2019-06-25 14:41:27 +1000

    Build 3.0

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Star-Wars-Match-Up/build/precache-manifest.d8565142b4f0b9e2d742108dfe657962.js"
+  "/Star-Wars-Match-up/Build/precache-manifest.8c2fc66930bf73460cda25062571f242.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Star-Wars-Match-Up/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Star-Wars-Match-up/Build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
